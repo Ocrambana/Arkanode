34a20b7b7d3d0b44c90a4264188fff87d1b6821e
Marco Coccato <coccato.m@gmail.com>
2020-02-18 15:40:15 +0100

    Added beveler class

---
+++ Engine/Beveler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Graphics.h"
+#include "RectF.h"
+
+class Beveler
+{
+public:
+	Beveler(Color baseColor);
+	void DrawOuterBevel(const RectF& rect, int size, Graphics & gfx) const;
+	void DrawInnerBevel(const RectF& rect, int size, Graphics & gfx) const;
+	void DrawBeveledBrick(const RectF& rect, int bevelSize, Graphics & gfx) const;
+	void DrawBevelFrame(const RectF& rect, int bevelSize, Graphics & gfx) const;
+	Color GetBaseColor() const;
+	void SetBaseColor(Color base);
+private:
+	static constexpr float	leftFactor = 1.10f,
+							topFactor = 0.93f,
+							bottomFactor = 0.80f,
+							rightFactor = 0.65f;
+	Color	baseColor,
+			topColor,
+			bottomColor,
+			leftColor,
+			rightColor;
+};
+
